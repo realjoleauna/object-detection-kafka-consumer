61e2303c73f5457a0898933b7d8f639c505fb1d3
juliana <>
2023-06-30 15:02:10 +0000

    Alterações
    Alterações para conexão

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ from kafka import KafkaConsumer, KafkaProducer
 from prediction import predict
 
 KAFKA_BOOTSTRAP_SERVER = os.getenv('KAFKA_BOOTSTRAP_SERVER')
-KAFKA_SECURITY_PROTOCOL = os.getenv('KAFKA_SECURITY_PROTOCOL')
-KAFKA_SASL_MECHANISM = os.getenv('KAFKA_SASL_MECHANISM')
-KAFKA_USERNAME = os.getenv('KAFKA_USERNAME')
-KAFKA_PASSWORD = os.getenv('KAFKA_PASSWORD')
+#KAFKA_SECURITY_PROTOCOL = os.getenv('KAFKA_SECURITY_PROTOCOL')
+#KAFKA_SASL_MECHANISM = os.getenv('KAFKA_SASL_MECHANISM')
+#KAFKA_USERNAME = os.getenv('KAFKA_USERNAME')
+#KAFKA_PASSWORD = os.getenv('KAFKA_PASSWORD')
 KAFKA_CONSUMER_GROUP = 'object-detection-consumer-group'
 KAFKA_CONSUMER_TOPIC = os.getenv('KAFKA_TOPIC_IMAGES')
 KAFKA_PRODUCER_TOPIC = os.getenv('KAFKA_TOPIC_OBJECTS')
@@ -23,19 +23,11 @@ def main():
     consumer = KafkaConsumer(KAFKA_CONSUMER_TOPIC,
                              group_id=KAFKA_CONSUMER_GROUP,
                              bootstrap_servers=KAFKA_BOOTSTRAP_SERVER,
-                             security_protocol=KAFKA_SECURITY_PROTOCOL,
-                             sasl_mechanism=KAFKA_SASL_MECHANISM,
-                             sasl_plain_username=KAFKA_USERNAME,
-                             sasl_plain_password=KAFKA_PASSWORD,
                              enable_auto_commit=False,
                              api_version_auto_timeout_ms=30000,
                              request_timeout_ms=450000)
 
     producer = KafkaProducer(bootstrap_servers=KAFKA_BOOTSTRAP_SERVER,
-                             security_protocol=KAFKA_SECURITY_PROTOCOL,
-                             sasl_mechanism=KAFKA_SASL_MECHANISM,
-                             sasl_plain_username=KAFKA_USERNAME,
-                             sasl_plain_password=KAFKA_PASSWORD,
                              api_version_auto_timeout_ms=30000,
                              max_block_ms=900000,
                              request_timeout_ms=450000,
